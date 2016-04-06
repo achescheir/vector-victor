716e86a8474481d3966ae09b7b90316ef1a2aea1
Alex Chescheir <alexchescheir@Nancys-MacBook-Pro.local>
2016-04-06 14:37:06 -0400

    Implemented shape() vector_add() and vecto_sub()

---
+++ linear_algebra.py
@@ -0,0 +1,24 @@
+
+class ShapeError(Exception):
+    pass
+
+def shape(matrix):
+    # columns = [len(x), for x in matrix]
+    # columns_length = min(columns):
+    #
+    # if columns is None:
+        return (len(matrix),)
+    # else
+    #     return
+
+def vector_add(vector1, vector2):
+    if shape(vector1) == shape(vector2):
+        return [x + y for x, y in zip(vector1, vector2)]
+    else:
+        raise ShapeError
+
+def vector_sub(vector1, vector2):
+    if shape(vector1) == shape(vector2):
+        return[x - y for x, y in zip(vector1, vector2)]
+    else:
+        raise ShapeError
